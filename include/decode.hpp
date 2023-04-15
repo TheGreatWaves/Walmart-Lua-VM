9cc93a117c20c63510f8b9354e6bfe2c139c6f89
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-04-15 16:36:15 +0700

    Abstracted args decoding

--- include/decode.hpp
+++ include/decode.hpp
@@ -4,6 +4,14 @@
 
 #include "instruction.hpp"
 
+#define GET(ARG) (instruction>>POS_##ARG) & MASK1(SIZE_##ARG, 0)
+
+#define ARGS(x) \
+  x(OP) \
+  x(A) \
+  x(B) \
+  x(C) 
+
 namespace walmart_lua
 {
   [[nodiscard]] auto get_op(uint8_t opcode) -> OpCode 
@@ -11,31 +19,14 @@ namespace walmart_lua
     return static_cast<OpCode>(opcode);
   }
 
-  [[nodiscard]] auto decode_opcode(uint32_t instruction) -> uint8_t 
-  {
-    return (instruction >> POS_OP) & MASK1(SIZE_OP, 0);
-  }
-
-  [[nodiscard]] auto decode_A(uint32_t instruction) -> uint8_t
-  {
-
-    return ((instruction >> POS_A) & MASK1(SIZE_A, 0));
-  }
-  
-  [[nodiscard]] auto decode_B(uint32_t instruction) -> uint8_t
-  {
-    return (instruction >> POS_B) & MASK1(SIZE_B, 0);
-  }
-
-  [[nodiscard]] auto decode_C(uint32_t instruction) -> uint8_t
-  {
-    return (instruction >> POS_C) & MASK1(SIZE_C, 0);
-  }
+  #define MAKE_DECODE_FN(arg) [[nodiscard]] auto decode_##arg(uint32_t instruction) -> uint8_t { return GET(arg); }
+  ARGS(MAKE_DECODE_FN)
+  #undef MAKE_DECODE_FN
 }
 
 struct Instr_ABC {uint8_t A; uint8_t B; uint8_t C; };
 
-#define decode_op(instr) walmart_lua::get_op(walmart_lua::decode_opcode(instr))
+#define decode_op(instr) walmart_lua::get_op(walmart_lua::decode_OP(instr))
 #define decode_ABC(instr) Instr_ABC { walmart_lua::decode_A(instr), walmart_lua::decode_B(instr), walmart_lua::decode_C(instr) }
 
 #endif /* WALMART_LUA_DECODE */
