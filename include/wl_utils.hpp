aadad7bd8ea12170fe8125ad8570e7fa72aaa932
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-04-15 15:30:25 +0700

    Minor bug, removed static from helper function signature

--- include/wl_utils.hpp
+++ include/wl_utils.hpp
@@ -5,8 +5,9 @@
 #include <sstream>
 #include <bitset>
 
+// https://stackoverflow.com/questions/7349689/how-to-print-using-cout-a-number-in-binary-form
 template<typename T>
-static std::string toBinaryString(const T& x)
+std::string toBinaryString(const T& x)
 {
     std::stringstream ss;
     ss << std::bitset<sizeof(T) * 8>(x);
